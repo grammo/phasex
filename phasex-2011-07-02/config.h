2ed4a2ee2b3754e060f73b1435f1de389bb21f1a
Your Name <linuxpeerjam@gmail.com>
2011-07-02 11:25:55 +0200

    add phasex sources plus jacksession support. initial upload.

---
+++ phasex-2011-07-02/config.h
@@ -0,0 +1,192 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Default installation prefix */
+#define CONFIG_PREFIX "/usr"
+
+/* GPLv2 License Text */
+#define GPLv2_LICENSE_FILE "/usr/share/doc/phasex-dev-m2/GPL-v2.txt"
+
+/* PHASEX Dark GTK Theme */
+#define GTKRC_DARK "/usr/share/themes/Phasex-Dark/gtk-2.0/gtkrc"
+
+/* PHASEX Light GTK Theme */
+#define GTKRC_LIGHT "/usr/share/themes/Phasex-Light/gtk-2.0/gtkrc"
+
+/* GTK engine directory */
+#define GTK_ENGINE_DIR "/usr/lib/gtk-2.0/2.10.0/engines"
+
+/* Define to 1 if you have the `atof' function. */
+#define HAVE_ATOF 1
+
+/* Define to 1 if you have the `atoi' function. */
+#define HAVE_ATOI 1
+
+/* Define to 1 if you have the `fclose' function. */
+#define HAVE_FCLOSE 1
+
+/* Define to 1 if you have the `fgets' function. */
+#define HAVE_FGETS 1
+
+/* Define to 1 if you have the `fopen' function. */
+#define HAVE_FOPEN 1
+
+/* Define to 1 if you have the `fprintf' function. */
+#define HAVE_FPRINTF 1
+
+/* Define to 1 if you have the `fread' function. */
+#define HAVE_FREAD 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `isdigit' function. */
+#define HAVE_ISDIGIT 1
+
+/* Define to 1 if you have the `isupper' function. */
+#define HAVE_ISUPPER 1
+
+/* Define to 1 if you have the `m' library (-lm). */
+#define HAVE_LIBM 1
+
+/* Define to 1 if your system has a GNU libc compatible `malloc' function, and
+   to 0 otherwise. */
+#define HAVE_MALLOC 1
+
+/* Define to 1 if you have the `memcpy' function. */
+#define HAVE_MEMCPY 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the `memset' function. */
+#define HAVE_MEMSET 1
+
+/* Define to 1 if you have the `poll' function. */
+#define HAVE_POLL 1
+
+/* libpthread has pthread_mutexattr_setprotocol */
+#define HAVE_PTHREAD_MUTEXATTR_SETPROTOCOL 1
+
+/* Define to 1 if you have the `sleep' function. */
+#define HAVE_SLEEP 1
+
+/* Define to 1 if you have the `snprintf' function. */
+#define HAVE_SNPRINTF 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the `strcasecmp' function. */
+#define HAVE_STRCASECMP 1
+
+/* Define to 1 if you have the `strcmp' function. */
+#define HAVE_STRCMP 1
+
+/* Define to 1 if you have the `strdup' function. */
+#define HAVE_STRDUP 1
+
+/* Define to 1 if you have the `strerror' function. */
+#define HAVE_STRERROR 1
+
+/* Define to 1 if you have the `strerror_r' function. */
+#define HAVE_STRERROR_R 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `strncmp' function. */
+#define HAVE_STRNCMP 1
+
+/* Define to 1 if you have the `strstr' function. */
+#define HAVE_STRSTR 1
+
+/* Define to 1 if you have the `strtok_r' function. */
+#define HAVE_STRTOK_R 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the `usleep' function. */
+#define HAVE_USLEEP 1
+
+/* System MidiMap Directory */
+#define MIDIMAP_DIR "/usr/share/phasex/sys-midimaps"
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+/* #undef NO_MINUS_C_MINUS_O */
+
+/* Name of package */
+#define PACKAGE "phasex"
+
+/* Address where bug reports should be sent */
+#define PACKAGE_BUGREPORT "assault64@gmail.com"
+
+/* Default installation data prefix */
+#define PACKAGE_DATA_DIR "/usr/share"
+
+/* Default installation locale prefix */
+#define PACKAGE_LOCALE_DIR "/usr/share/locale"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "PHASEX"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "PHASEX dev-m2"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "phasex"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "dev-m2"
+
+/* Parameter Help File */
+#define PARAM_HELPFILE "/usr/share/phasex/help/parameters.help"
+
+/* System Patch Directory */
+#define PATCH_DIR "/usr/share/phasex/sys-patches"
+
+/* PHASEX Data Directory */
+#define PHASEX_DIR "/usr/share/phasex"
+
+/* GTKRC stub for theme engine */
+#define PHASEX_GTK_ENGINE_RC "/usr/share/phasex/gtkenginerc"
+
+/* PHASEX License Text */
+#define PHASEX_LICENSE_FILE "/usr/share/doc/phasex-dev-m2/LICENSE"
+
+/* Pixmap Directory */
+#define PIXMAP_DIR "/usr/share/phasex/pixmaps"
+
+/* System Sample Directory */
+#define SAMPLE_DIR "/usr/share/phasex/sys-samples"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "dev-m2"
+
+/* Build without LASH support */
+/* #undef WITHOUT_LASH */
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define to rpl_malloc if the replacement function should be used. */
+/* #undef malloc */
